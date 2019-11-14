6a829d6fc672e95a2d5c89cba5431d80e1756eef
Devit951 <devit951@gmail.com>
2019-11-14 15:28:53 +0500

    Added more magic tips

--- app/src/main/java/devit951/github/magictipexample/MainActivity.kt
+++ app/src/main/java/devit951/github/magictipexample/MainActivity.kt
@@ -2,17 +2,15 @@ package devit951.github.magictipexample
 
 import android.annotation.TargetApi
 import android.graphics.Color
-import android.graphics.Outline
 import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.ViewOutlineProvider
 import androidx.appcompat.app.AppCompatActivity
-import devit951.github.magictip.tip.AutoCloseMagicTip
 import devit951.github.magictip.MagicTip
-import devit951.github.magictip.tip.OneMagicTip
 import devit951.github.magictip.animationdelegate.CombinedMagicTipAnimation
+import devit951.github.magictip.animationdelegate.OvershootMagicTipAnimation
 import devit951.github.magictip.animationdelegate.ScaledTranslateMagicTipAnimation
+import devit951.github.magictip.tip.AutoCloseMagicTip
+import devit951.github.magictip.tip.OneMagicTip
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,25 +19,50 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tv_test.setOnClickListener { anchorView ->
-            AutoCloseMagicTip(anchorView)
-                .settings {
-                    text = "Lorem Posen"
-                    bgColor = Color.CYAN
-                }
-                .show()
+        tv_magictip.setOnClickListener { anchorView ->
+            showMagicTip {
+                MagicTip(anchorView)
+                    .settings {
+                        text = "Lorem Posen"
+                        bgColor = Color.RED
+                    }
+            }
         }
-        imageView.setOnClickListener {
-            OneMagicTip(it, MagicTip(it)
-                .settings {
-                    startAnimationDelegate =
-                        CombinedMagicTipAnimation(ScaledTranslateMagicTipAnimation())
-                    exitAnimationDelegate = ScaledTranslateMagicTipAnimation.Reversed()
-                    text = "Lorem Posen"
-                    bgColor = Color.BLUE
+        tv_auto_close_magictip.setOnClickListener {
+            showMagicTip {
+                AutoCloseMagicTip(it)
+                    .settings {
+                        text = "Lorem Posen"
+                        bgColor = Color.GREEN
+                    }
+            }
+        }
+        iv_animated_magictip.setOnClickListener {
+            showMagicTip {
+                OneMagicTip(it, MagicTip(it)
+                    .settings {
+                        startAnimationDelegate =
+                            CombinedMagicTipAnimation(ScaledTranslateMagicTipAnimation())
+                        exitAnimationDelegate = ScaledTranslateMagicTipAnimation.Reversed()
+                        text = "Lorem Posen"
+                        bgColor = Color.BLUE
+                    })
+            }
+        }
+        tv_overshoot_magictip.setOnClickListener {
+            showMagicTip {
+                MagicTip(it)
+                    .settings {
+                        startAnimationDelegate = OvershootMagicTipAnimation()
+                        exitAnimationDelegate = OvershootMagicTipAnimation.Reversed()
+                        text = "Lorem Posen"
+                        bgColor = Color.BLACK
+                    }
+            }
+        }
+    }
 
-                })
-                .show()
-        }
+    private fun showMagicTip(magicTip: () -> MagicTip) {
+        magicTip.invoke().show()
     }
 }
